72c55ccb64e420235bbc2cc5bb654d254b287d90
B6121853 <amp.nonthawan.g7@gmail.com>
2022-01-05 19:27:20 +0700

    save

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,54 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
+  <v-app>
+    <div>
+      <v-card class="mx-auto overflow-hidden" height="auto">
+        <v-app-bar color="deep-purple" dark>
+          <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
+          <v-toolbar-title>Stock control & Inventory</v-toolbar-title>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+          <v-list nav dense>
+            <v-list-item-group
+              v-model="group"
+              active-class="deep-purple--text text--accent-4"
+            >
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Home</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-account</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Account</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
+        <router-view />
+      </v-card>
+      <!-- <div id="app">
+        <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+        <router-view />
+      </div> -->
+    </div>
+  </v-app>
+</template>
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -16,7 +57,6 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 
